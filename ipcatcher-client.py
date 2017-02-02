b52835a26cbb74a5e27c0143c1b1036465c962fb
T.J. Simmons <tj@tjsimmons.net>
2017-02-01 19:55:20 -0600

    print the date for logging

--- ipcatcher-client.py
+++ ipcatcher-client.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 import requests
 import os
+from datetime import datetime
 
 key = os.environ['IPCATCHER_KEY']
 
 r = requests.post('http://ipcatcher.tjsimmons.net/services/', data={'key': key})
 
+print datetime.now()
 print(r.text)
 print(r.status_code)
+print('\n')
