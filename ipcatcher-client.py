c6d26eaba62748240eaaabbcf504d39f8b034291
T.J. Simmons <tj@tjsimmons.net>
2017-02-01 19:56:26 -0600

    parens

--- ipcatcher-client.py
+++ ipcatcher-client.py
@@ -7,7 +7,7 @@ key = os.environ['IPCATCHER_KEY']
 
 r = requests.post('http://ipcatcher.tjsimmons.net/services/', data={'key': key})
 
-print datetime.now()
+print(datetime.now())
 print(r.text)
 print(r.status_code)
 print('\n')
